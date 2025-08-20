3cab9392dace9f6cbf7c3e7664ed0915917d8759
Siddy2611 <siddy2611@gmail.com>
2025-08-20 19:38:42 +0530

    Updated

--- script.js
+++ script.js
@@ -231,13 +231,6 @@ document.querySelectorAll(".take-what-you-need .tab").forEach(tab => {
 });
 
 
-// In script.js at the bottom
-window.addEventListener("pageshow", () => {
-  document.querySelectorAll(".take-what-you-need .tab").forEach(tab => {
-    tab.style.opacity = "1";
-    tab.style.transform = "rotate(180deg)";
-  });
-});
 
 // start
 setTimeout(showFace, 1800);
